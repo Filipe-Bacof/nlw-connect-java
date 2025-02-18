32ed425fbbe96d493d4fdc573aa8a73614df5893
Filipe-Bacof <filipebacof@gmail.com>
2025-02-17 22:36:21 -0300

    feat: first commit

---
+++ src/test/java/br/com/bacof/nlw_connect/NlwConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bacof.nlw_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
