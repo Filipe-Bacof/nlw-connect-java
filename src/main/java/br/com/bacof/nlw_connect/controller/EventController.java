5ed529785689a04314009eb89800c620ec7a11df
Filipe-Bacof <filipebacof@gmail.com>
2025-02-18 19:52:52 -0300

    feat: endpoints get

--- src/main/java/br/com/bacof/nlw_connect/controller/EventController.java
+++ src/main/java/br/com/bacof/nlw_connect/controller/EventController.java
@@ -1,6 +1,11 @@
 package br.com.bacof.nlw_connect.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +22,20 @@ public class EventController {
 	@PostMapping("/events")
 	public Event addNewEvent(@RequestBody Event newEvent) {
 		return service.addNewEvent(newEvent);
+	}
+	
+	@GetMapping("/events")
+	public List<Event> getAllEvents() {
+		return service.getAllEvents();
+	}
+	
+	@GetMapping("/events/{prettyName}")
+	public ResponseEntity<Event> getEventByPrettyName(@PathVariable String prettyName) {
+		Event event = service.getByPrettyName(prettyName);
+		
+		if (event != null) {
+			return ResponseEntity.ok().body(event);
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
